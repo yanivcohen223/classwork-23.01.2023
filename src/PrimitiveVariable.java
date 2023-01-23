d60943bbdf36b9ab58f61568396ccc2a262df8ce
yanivcohen223 <yanivcohen223@gmail.com>
2023-01-23 21:45:06 +0200

    upload

--- src/PrimitiveVariable.java
+++ src/PrimitiveVariable.java
@@ -8,14 +8,23 @@ public class PrimitiveVariable {
         float myFloat = -7.21f;
         double myDouble = 3.142d ;
 
+
+
         //ex2
         float myFloat1 = myByte;
         int myInt1 = (int) myDouble;
+        double myDouble2 = myFloat;
+        double myDouble3 = myInt;
+        byte myByte2 = (byte) myShort;
 
         System.out.println(myFloat1);
         System.out.println(myInt1);
+        System.out.println(myDouble2);
+        System.out.println(myDouble3);
+        System.out.println(myByte2);
+
 
         //ex3
-        int myInt2 = int myDouble;
+        //int myInt2 = int myDouble;
     }
 }
